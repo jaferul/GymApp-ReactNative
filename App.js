2f7c8854e2aa82d358724e2e59aa2e84a5a0add8
Jafer <nusierj@gmail.com>
2021-09-19 18:55:01 +0100

    Implemented navigation between screens/ (use useNavigation() to use the prop in another file)

--- App.js
+++ App.js
@@ -6,20 +6,30 @@ import Food from './screens/food';
 import Music from './screens/music';
 import Progress  from './screens/progress';
 import Home from './screens/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+ const App = () => {
   return (
-    <View style = {styles.container}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Music" component={Music} />
+        <Stack.Screen name="Food" component={Food} />
+        <Stack.Screen name="Workouts" component={Workouts} />
+        <Stack.Screen name="Progress" component={Progress} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    //flexDirection: 'row',
     flex: 1,
     backgroundColor: '#fff',
   },
 });
+
+export default App;
--- components-homeScreen/body.js
+++ components-homeScreen/body.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
 
 export default function Body(){
+    const navigation = useNavigation();
+
     return(
         <View style = {styles.buttons}>   
 
-            <TouchableOpacity onPress={() => {alert('You tapped the workout button!');}}>
+            <TouchableOpacity onPress={() => navigation.navigate('Workouts')}>
                 <View  style = {styles.item}>
                     <MaterialIcons name = 'fitness-center' size = {20} />
                     <Text>Workouts</Text>
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => {alert('You tapped the music button!');}}>
+            <TouchableOpacity onPress={() => navigation.navigate('Music')}>
                 <View  style = {styles.item}>
                     <MaterialIcons name = 'music-note' size = {20} />
-                    <Text>Music</Text>          
+                    <Text>Music</Text>    
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => {alert('You tapped the progress button!');}}>
+            <TouchableOpacity onPress={() => navigation.navigate('Progress')}>
                 <View  style = {styles.item}>
                     <MaterialIcons name = 'add-to-photos' size = {20} />
                     <Text>Progress</Text>          
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => {alert('You tapped the food button!');}}>
+            <TouchableOpacity onPress={() => navigation.navigate('Food')}>
                 <View  style = {styles.item}>
                     <MaterialIcons name = 'food-bank' size = {20}/>
                     <Text>Food</Text>
